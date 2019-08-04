96fa4fc342d509b66a51cd67ac5af533ce01d9ff
YOLO R <52312596+haydenledean@users.noreply.github.com>
2019-08-04 11:33:08 +0200

    Update server.js

--- js/server.js
+++ js/server.js
@@ -1,3 +1,3 @@
 //var WEBSOCKET_SERVER = "ws://127.0.0.1:9000/royale/ws";
-var WEBSOCKET_SERVER = "ws://marioroyale.cyuubi.gq:9000/royale/ws";
+var WEBSOCKET_SERVER = "ws://haydenledean.github.io/mario-royale:9000/royale/ws";
 print("loading server.js finished");
